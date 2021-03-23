86a1344480a9f946907dddcee57e6b713b1734b1
Israel Andrade <israelg.andrade@bol.com.br>
2021-03-23 14:22:26 -0300

    Obtendo usuário por id

--- src/main/java/com/israelandrade/workshopmongo/services/UserService.java
+++ src/main/java/com/israelandrade/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.israelandrade.workshopmongo.services;
 
 import com.israelandrade.workshopmongo.domain.User;
 import com.israelandrade.workshopmongo.repositories.UserRepository;
+import com.israelandrade.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
         return repository.findAll();
     }
 
+    public Optional<User> findById(String id) {
+        Optional<User> user = repository.findById(id);
+        if(user == null){
+            throw  new ObjectNotFoundException("Objeto não encotrado");
+        }
+        return user;
+    }
 }
