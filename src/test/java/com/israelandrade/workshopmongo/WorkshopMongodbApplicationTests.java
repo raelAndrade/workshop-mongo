5d95375f8a0f69128812ac8c4fb992ae4e0ebf32
Israel Andrade <israelg.andrade@bol.com.br>
2021-03-22 23:29:44 -0300

    Projeto criado

---
+++ src/test/java/com/israelandrade/workshopmongo/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelandrade.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
