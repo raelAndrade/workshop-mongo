f68e19b23c3aa5b9eaa1a663db793c5571558f60
Israel Andrade <israelg.andrade@bol.com.br>
2021-03-23 00:35:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/israelandrade/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.israelandrade.workshopmongo.repositories;
+
+import com.israelandrade.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
